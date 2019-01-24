1fb7604a7d44ed6eca345c88c1a19746396c22c7
aaay-aaay <aaayaaay58@gmail.com>
2019-01-24 14:39:27 +0000

    Add FAtlas 'patch'

---
+++ RainbowOverhaul/FAtlasPatch.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+
+namespace Rainbow
+{
+    public static class FAtlasPatch
+    {
+        public static void Patch()
+        {
+        }
+        
+        public static void LoadAtlasDataFromString(this FAtlas atlas, string data)
+        {
+            Dictionary<string, object> dictionary = data.dictionaryFromJson();
+            if (dictionary == null)
+            {
+                throw new FutileException("This data is not a proper JSON file. Make sure to select \"Unity3D\" in TexturePacker.");
+            }
+            Dictionary<string, object> dictionary2 = (Dictionary<string, object>)dictionary["frames"];
+            float resourceScaleInverse = Futile.resourceScaleInverse;
+            int num = 0;
+            foreach (KeyValuePair<string, object> keyValuePair in dictionary2)
+            {
+                FAtlasElement fatlasElement = new FAtlasElement();
+                fatlasElement.indexInAtlas = num++;
+                string text = keyValuePair.Key;
+                if (Futile.shouldRemoveAtlasElementFileExtensions)
+                {
+                    int num2 = text.LastIndexOf(".");
+                    if (num2 >= 0)
+                    {
+                        text = text.Substring(0, num2);
+                    }
+                }
+                fatlasElement.name = text;
+                IDictionary dictionary3 = (IDictionary)keyValuePair.Value;
+                fatlasElement.isTrimmed = (bool)dictionary3["trimmed"];
+                if ((bool)dictionary3["rotated"])
+                {
+                    throw new NotSupportedException("Futile no longer supports TexturePacker's \"rotated\" flag. Please disable it when creating the atlas.");
+                }
+                IDictionary dictionary4 = (IDictionary)dictionary3["frame"];
+                float num3 = float.Parse(dictionary4["x"].ToString());
+                float num4 = float.Parse(dictionary4["y"].ToString());
+                float num5 = float.Parse(dictionary4["w"].ToString());
+                float num6 = float.Parse(dictionary4["h"].ToString());
+                Rect uvRect = new Rect(num3 / atlas._textureSize.x, (atlas._textureSize.y - num4 - num6) / atlas._textureSize.y, num5 / atlas._textureSize.x, num6 / atlas._textureSize.y);
+                fatlasElement.uvRect = uvRect;
+                fatlasElement.uvTopLeft.Set(uvRect.xMin, uvRect.yMax);
+                fatlasElement.uvTopRight.Set(uvRect.xMax, uvRect.yMax);
+                fatlasElement.uvBottomRight.Set(uvRect.xMax, uvRect.yMin);
+                fatlasElement.uvBottomLeft.Set(uvRect.xMin, uvRect.yMin);
+                IDictionary dictionary5 = (IDictionary)dictionary3["sourceSize"];
+                fatlasElement.sourcePixelSize.x = float.Parse(dictionary5["w"].ToString());
+                fatlasElement.sourcePixelSize.y = float.Parse(dictionary5["h"].ToString());
+                fatlasElement.sourceSize.x = fatlasElement.sourcePixelSize.x * resourceScaleInverse;
+                fatlasElement.sourceSize.y = fatlasElement.sourcePixelSize.y * resourceScaleInverse;
+                IDictionary dictionary6 = (IDictionary)dictionary3["spriteSourceSize"];
+                float left = float.Parse(dictionary6["x"].ToString()) * resourceScaleInverse;
+                float top = float.Parse(dictionary6["y"].ToString()) * resourceScaleInverse;
+                float width = float.Parse(dictionary6["w"].ToString()) * resourceScaleInverse;
+                float height = float.Parse(dictionary6["h"].ToString()) * resourceScaleInverse;
+                fatlasElement.sourceRect = new Rect(left, top, width, height);
+                atlas._elements.Add(fatlasElement);
+                atlas._elementsByName.Add(fatlasElement.name, fatlasElement);
+                Debug.Log(fatlasElement.name);
+                
+                fatlasElement.atlas = atlas;
+                Futile.atlasManager.AddElement(fatlasElement);
+                
+            }
+        }
+    }
+}
